b5bfc5ab8703cbcf98d035cf704811430e4ac2ba
Sammed Akiwate <40512123+Samaki1008@users.noreply.github.com>
2022-07-19 14:57:20 +0530

    Update Sammed.py

--- Sammed.py
+++ Sammed.py
@@ -1,2 +1,39 @@
 import json
 
+class RCB:
+  f = open('data.json')
+  data = json.load(f)
+  count=0
+  wicketkeeper=False
+  
+      
+  def Question1():
+    for i in data['player']:
+      if(i['country']!=['India']):
+        count++
+  
+    assertFlag = assertTrue(count==4)
+    if(assertFlag):
+      print("True")
+    else:
+      print("False")
+    
+  def Question2():
+    for i in data['player']:
+      if(i['role']==['Wicket-keeper']):
+        wicketkeeper=True
+  
+    
+    if(wicketkeeper):
+      print("True")
+    else:
+      print("False")
+    
+    
+    
+  f.close()
+  
+sam = RCB()
+print("First question", sam.Question1())
+print("2nd  question", sam.Question2())
+
